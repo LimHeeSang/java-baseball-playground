30956eb68d9f3259a79625a38480a30f062654a7
heesang <dlagmltkd12@naver.com>
2022-02-22 04:07:58 +0900

    test : split 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,4 +11,22 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        //given
+        String str = "1,2";
+        String str2 = "1";
+
+        //when
+        String[] splited = str.split(",");
+        String[] splited2 = str2.split(",");
+
+        //then
+        Assertions.assertThat(splited[0]).isEqualTo("1");
+        Assertions.assertThat(splited[1]).isEqualTo("2");
+
+        Assertions.assertThat(splited2[0]).isEqualTo("1");
+    }
+
 }
