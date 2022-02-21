e7b582db4f32558b2f5bf34a84e730e84e825795
heesang <dlagmltkd12@naver.com>
2022-02-22 05:20:38 +0900

    test : set collection contains 학습 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
