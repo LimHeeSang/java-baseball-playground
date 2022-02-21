877f0d13b189a9fc68570f9931960974c437add9
heesang <dlagmltkd12@naver.com>
2022-02-22 05:11:18 +0900

    test : set collection contains 학습 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -23,5 +25,12 @@ public class SetTest {
     void size() {
         Assertions.assertThat(numbers.size()).isEqualTo(3);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        org.junit.jupiter.api.Assertions.assertTrue(numbers.contains(number));
+    }
+
 
 }
