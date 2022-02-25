12af15ebbae9251f66bbfb42d545c9e3f7e88657
heesang <dlagmltkd12@naver.com>
2022-02-26 03:32:54 +0900

    feat : 숫자야구게임 실행 클래스

---
+++ src/main/java/PlayNumberBaseballGame.java
@@ -0,0 +1,6 @@
+public class PlayNumberBaseballGame {
+    public static void main(String[] args) {
+        NumberBaseballGame baseballGame = new NumberBaseballGame();
+        baseballGame.operation();
+    }
+}
